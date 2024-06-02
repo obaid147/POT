be80fb9e418c070f4d57898f8aff0ede81df4605
obaid147 <obaidfayazwani@gmal.com>
2024-06-02 16:17:49 +0400

    Home Page changed to a single page for  create and view records

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    #path("", views.index, name="index"),
+    path("", views.create_purchase_order, name="create_purchase_order"),
     path("create/", views.create_purchase_order, name="create_purchase_order"),
     path("signup/", views.signup, name="signup")
 ]
